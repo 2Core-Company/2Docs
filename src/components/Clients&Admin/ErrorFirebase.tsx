f22086550d4a4ceb7f106f5912d2ed8c690c8df6
Lucas TKP <lucasgeansantos@gmail.com>
2023-02-06 10:14:57 -0300

    fixed: resolvendo conflitos do merge

--- src/components/Clients&Admin/ErrorFirebase.tsx
+++ src/components/Clients&Admin/ErrorFirebase.tsx
@@ -3,10 +3,10 @@ import {toast} from 'react-toastify'
 function ErrorFirebase(props:{message?:string, code?:string}) {
     const errors = props
     var message = errors.message
-      if(errors.code === "auth/too-many-requests"){
-        message = "Limite de tentativas de login excedido, tente novamente mais tarde."
+      if(errors.code === "auth/too-many-requests"){        
+        message = "Limite de tentativas de login excedido, tente novamente mais tarde."        
       } else if (errors.code === "auth/wrong-password"){
-        message = "Sua senha está incorreta!"
+        message = "Sua senha está incorreta."
       } else if(errors.code === "auth/user-not-found"){
         message = "Este usuário não foi cadastrado."
       } else if(errors.code === "auth/email-already-in-use"){
@@ -14,11 +14,12 @@ function ErrorFirebase(props:{message?:string, code?:string}) {
       } else if(errors.code === "auth/email-already-exists"){
         message = "Já existe um usuário cadastrado com este email." 
       } else if( errors.code === "auth/user-disabled"){
-        message = "Este usuário foi desabilidado"
+        message = "Este usuário foi desabilitado."
       } else if( errors.code === "auth/invalid-email"){
         message = "O formato de email digitado não é aceito pelo nosso sistema."
       }
-      toast.error(message)
+      toast.error(message);
+      throw new Error(message);
 }
 
 export default ErrorFirebase
