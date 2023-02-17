838b2a604c40041e772e92eb7f5ce973038ebb40
BuildTools <unconfigured@null.spigotmc.org>
2023-02-17 14:27:36 -0300

    Feat: Added DarkMode to Enterprise Switch

--- src/components/Clients&Admin/Enterprise/index.tsx
+++ src/components/Clients&Admin/Enterprise/index.tsx
@@ -20,15 +20,15 @@ function Enterprises({enterprises, enterprise, user, setUser, setEnterprise, fro
     const [createEnterprises, setCreateEnterprises] = useState(false)
 
   return (
-        <div className='absolute right-[20px] top-[10px] bg-neutral-200 border-[2px] border-black rounded-[4px] pt-[3px]'>
+        <div className='absolute right-[20px] top-[10px] bg-neutral-200 dark:bg-neutral-200/20 border-[2px] border-black dark:border-white rounded-[4px] pt-[3px]'>
             {createEnterprises ? <CreateEnterprises user={user} setUser={setUser} setCreateEnterprises={setCreateEnterprises}/> : <></>}
             <div className='flex items-center px-[7px] justify-between' onClick={() => setChangeEnterprise(!changeEnterprise)}>
                 <p className='max-w-[150px] overflow-hidden text-ellipsis' >{enterprise.name}</p>
                 <div className='flex ml-[10px] '>
-                    <div className='w-[20px] h-[20px] rounded-full border-black p-[2px] border-[2px]'>
-                        <div className='bg-black w-full h-full rounded-full'></div>
+                    <div className='w-[20px] h-[20px] rounded-full border-black dark:border-white p-[2px] border-[2px]'>
+                        <div className='bg-black dark:bg-white w-full h-full rounded-full'></div>
                     </div>
-                    <Image src={Arrow} alt="flecha" className={`w-[15px] ml-[5px] duration-200 ¨${changeEnterprise ? " rotate-180" : ""}`}/>
+                    <Image src={Arrow} alt="flecha" className={`w-[15px] ml-[5px] duration-200 dark:invert ¨${changeEnterprise ? " rotate-180" : ""}`}/>
                 </div>
             </div>
 
@@ -39,7 +39,7 @@ function Enterprises({enterprises, enterprise, user, setUser, setEnterprise, fro
                         <div key={data.id} className="flex itens-center mt-[5px] justify-between px-[7px]">
                             <p onClick={() => (setEnterprise(enterprises[index]), setChangeEnterprise(false))} className="cursor-pointer w-[100%] max-w-[150px] overflow-hidden text-ellipsis">{data.name}</p>
                             {from === "user" ? 
-                                <div onClick={() => (setEnterprise(enterprises[index]), setChangeEnterprise(false))} className='cursor-pointer min-w-[20px] h-[20px] rounded-full border-black p-[2px] border-[2px]' />
+                                <div onClick={() => (setEnterprise(enterprises[index]), setChangeEnterprise(false))} className='cursor-pointer min-w-[20px] h-[20px] rounded-full border-black dark:border-white p-[2px] border-[2px]' />
                             : 
                                 <Options user={user} index={index} setUser={setUser}/>
                             }
@@ -47,7 +47,7 @@ function Enterprises({enterprises, enterprise, user, setUser, setEnterprise, fro
                     )
                 })}
 
-                <button onClick={() => setCreateEnterprises(true)} className='flex items-center text-center w-full justify-center mt-[10px] hover:bg-neutral-300 border-t-[2px] cursor-pointer border-black'>
+                <button onClick={() => setCreateEnterprises(true)} className='flex items-center text-center w-full justify-center mt-[10px] hover:bg-neutral-300 dark:hover:bg-neutral-300/10 border-t-[2px] cursor-pointer border-black dark:border-white'>
                     <p>Criar</p>
                 </button>
             </div>
