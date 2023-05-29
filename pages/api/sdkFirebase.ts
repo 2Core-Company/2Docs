abe1453ee756609c38267dc8acda5d66b3ab1de0
Lucas TKP <lucasgeansantos@gmail.com>
2023-05-29 15:56:13 -0300

    fix: fixed bug delet User and not delet your files

--- pages/api/sdkFirebase.ts
+++ pages/api/sdkFirebase.ts
@@ -5,12 +5,10 @@ var serviceAccount = require("./serviceAccountKey.json");
 
 import { getAuth } from 'firebase-admin/auth'
 
-    if(!admin.apps.length) {
-        admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount),
-            databaseURL: "https://docs-dc26e-default-rtdb.firebaseio.com"
-        });
-    }
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: "https://docs-dc26e-default-rtdb.firebaseio.com"
+    });
     export const storage = new Storage({projectId:'docs-dc26e'});
     export const db = admin.firestore();
 
