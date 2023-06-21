ec382c23202bce793ff279db2383cb834a2484d2
Lucas TKP <lucasgeansantos@gmail.com>
2023-06-21 17:07:15 -0300

    refactore: redisign of navbar

--- src/components/Clients&Admin/NavBar/ModalExit.tsx
+++ src/components/Clients&Admin/NavBar/ModalExit.tsx
@@ -1,19 +1,18 @@
 interface Props {
-  message:string
   setModal:Function
   childModal:Function
 }
 
-function Modal({message, childModal, setModal}:Props) {
+function Modal({childModal, setModal}:Props) {
   return (
     <div className='w-screen h-screen fixed bg-black/40 backdrop-blur-[4px] flex justify-center items-center text-black dark:text-white z-50 top-[0px] left-0'>
       <div className='bg-primary dark:bg-dprimary w-[500px] max-lsm:w-[320px] rounded-[4px] flex flex-col'>
         <div  className='bg-red w-full h-[15px] rounded-t-[4px]'/>
         <div className=' px-[10px] text-left'>
-          <p className='text-[26px] mt-[10px]'>{message}</p>
+          <p className='text-[26px] mt-[10px]'>Tem certeza que deseja sair da sua conta?</p>
         </div>
         <div className='flex w-full justify-end gap-4 bg-hilight dark:bg-dhilight self-end  pr-[10px] py-[10px] rounded-b-[4px] mt-[25px]'>
-          <button onClick={() => setModal({status: false, message: ""})} className='bg-strong dark:bg-dstrong hover:scale-[1.10] duration-300 p-[5px] rounded-[8px] text-[20px] text-white cursor-pointer'>Cancelar</button>
+          <button onClick={() => setModal(false)} className='bg-strong dark:bg-dstrong hover:scale-[1.10] duration-300 p-[5px] rounded-[8px] text-[20px] text-white cursor-pointer'>Cancelar</button>
           <button onClick={() => childModal()} className='bg-red/40 border-2 border-red hover:scale-[1.10]  duration-300 p-[5px] rounded-[8px] text-[20px] text-white cursor-pointer'>Confirmar</button>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -25,50 +25,21 @@ module.exports = {
         secondary: "#AAAAAA",
         terciary: "#9E9E9E",
         strong: "#8F8F8F",
-        hilight: "#D9D9D9",
+        hilight: "#10B981",
         red: "#BD0303",
         greenV: "#087E14",
         blue: "#005694",
         //!Dark Theme
-        dprimary:"#181818",
-        dsecondary: "#494949",
-        dterciary: "#5C5C5C",
-        dstrong: "#8F8F8F",
-        dhilight: "#121212",
-        dred: "#BD0303",
-        dgreenV: "#087E14",
-        dblue: "#005694",
+        dprimary:"#262626",
+        dsecondary: "#181818",
+        dstrong: "##FFFFFF",
+        dhilight: "#10B981",
+        dred: "#BC2C1A",
+        dblue: "#2E86AB",
         dstrong: "#333333",
 
         //!Radix
         ...blackA,
-      },
-
-      keyframes: {
-        slideDownAndFade: {
-          from: { opacity: 0, transform: 'translateY(-2px)' },
-          to: { opacity: 1, transform: 'translateY(0)' },
-        },
-        slideLeftAndFade: {
-          from: { opacity: 0, transform: 'translateX(2px)' },
-          to: { opacity: 1, transform: 'translateX(0)' },
-        },
-        slideUpAndFade: {
-          from: { opacity: 0, transform: 'translateY(2px)' },
-          to: { opacity: 1, transform: 'translateY(0)' },
-        },
-        slideRightAndFade: {
-          from: { opacity: 0, transform: 'translateX(-2px)' },
-          to: { opacity: 1, transform: 'translateX(0)' },
-        },
-      },
-      
-      animation: {
-        'spin-slow': 'spin 3s linear infinite',
-        slideDownAndFade: 'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
-        slideLeftAndFade: 'slideLeftAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
-        slideUpAndFade: 'slideUpAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
-        slideRightAndFade: 'slideRightAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
       },
     },
 
