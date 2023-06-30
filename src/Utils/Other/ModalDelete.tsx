fc4e4755ae29b9a6ea8c7bbbce02042eb3cf808e
Lucas TKP <lucasgeansantos@gmail.com>
2023-06-30 17:11:45 -0300

    design: add box recent files and box upload

--- src/Utils/Other/ModalDelete.tsx
+++ src/Utils/Other/ModalDelete.tsx
@@ -11,7 +11,6 @@ interface Props {
   
   function ModalDelete({modal, childModal, setModal}:Props) {
     const [textConfirmation, setTextConfirmation] = useState('')
-''
     function VerifyConfirmation(){
       if(textConfirmation === 'confirmar'){
           childModal()
