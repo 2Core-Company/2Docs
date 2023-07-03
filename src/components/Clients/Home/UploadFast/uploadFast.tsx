4c1a703255a70ae7c311db037f15d92282915e9e
Lucas TKP <lucasgeansantos@gmail.com>
2023-07-03 10:13:21 -0300

    feature: add modal of radix in upload Fast of client

--- src/components/Clients/Home/UploadFast/uploadFast.tsx
+++ src/components/Clients/Home/UploadFast/uploadFast.tsx
@@ -19,13 +19,13 @@ function UploadFast() {
 
   return (
     <div className='self-end'>
-      {files && <ModalSelectUpload setFiles={setFiles} files={files}/>}
       <p className='mt-[20px] font-poiretOne text-[40px] max-sm:text-[30px]'>Upload Rápido</p>
       <label onDrop={handleDrop} onDragOver={handleDragOver} className='cursor-pointer hover:bg-[#e4e4e4] bg-primary border-dashed border-[1px] border-[#9E9E9E] rounded-[12px] w-[500px] max-sm:w-[390px] max-lsm:w-[340px] h-[250px] drop-shadow-[0_5px_5px_rgba(0,0,0,0.20)] flex flex-col items-center justify-center'>
           <UploadIcon className='text-[#9E9E9E] w-[48px] h-[56px]'/>
           <p className='text-[20px] max-sm:text-[18px] text-center'>Arraste um arquivo ou faça um <span className='text-hilight underline'>upload</span></p>
           <input onChange={(e) => VerifyFiles({files:e.target.files, setFiles})} multiple={true} type="file" name="document" id="document" className='hidden w-full h-full' />
       </label>
+      <ModalSelectUpload setFiles={setFiles} files={files}/>
     </div>
   )
 }
