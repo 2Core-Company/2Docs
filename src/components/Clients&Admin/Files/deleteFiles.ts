63d618a769cb19de078ec2f9dbff8e4381eef0a1
Yan Victor <ybritogomes@gmail.com>
2023-08-11 17:12:22 -0300

    fix(deletefiles.ts): fixed deletion of sharedFile document when deleting a user and a event

--- src/components/Clients&Admin/Files/deleteFiles.ts
+++ src/components/Clients&Admin/Files/deleteFiles.ts
@@ -18,6 +18,11 @@ async function deleteFiles({ files, selectedFiles, id_company }:Props) {
   try{
     if(selectedFiles.length > 0) {
       for await (const file of selectedFiles){
+        if(file.id_share) {
+          const shareRef = doc(db, 'files', file.id_company, file.id_user, 'user', 'sharedFiles', file.id_share);
+          batch.delete(shareRef);
+        }
+        
         const ref1 = doc(db, 'files', file.id_company, file.id_user, 'user', 'files', file.id);
         const desertRef = ref(storage, file.path);
         promises.push(deleteObject(desertRef));
